a09425e50237d3698e8816f5c6027f01c96aef82
NdubuisiO <onyemenamndu@gmail.com>
2018-09-10 21:48:13 +0100

    set up initial files

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php 
+require 'vendor/autoload.php'; 
+use  App\VerifyBot;
+use App\ReceivedMessage;
+use App\SendMessage;
+use DialogFlow\Client;
+
+
+$verifyBot = new VerifyBot();
+
+
+
+
+ 
+$input = json_decode(file_get_contents('php://input'), true);
+
+
+
+
+$receivedMessage = new ReceivedMessage($input);
+$sendMessage = new SendMessage($input);
+
+ 
+if(!empty($receivedMessage->textMessage)) {   
+
+	 $response = callDialogflow($receivedMessage->textMessage);
+     $sendMessage->text($response);
+     
+}
+
+function callDialogflow($textMessage) {
+
+	try {
+
+	    $client = new Client('4c6e0a277e274cf3953e9d984caaf8f5');
+
+	    $query = $client->get('query', [
+	        'query' => $textMessage,
+	        'sessionId' => time(),
+	    ]);
+
+	    $response = json_decode((string) $query->getBody(), true);
+	    $response =  $response['result']['fulfillment']['speech'];
+
+	    return $response;
+
+
+    } catch (\Exception $error) {
+    
+	    error_log($error);
+	}
+
+}
+
+
+
+ 
+
+
+
+ 
